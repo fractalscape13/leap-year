703771f831e67a146610212f95d9448c907bb8be
Joseph Wangemann <fractalscape13@gmail.com>
2019-01-23 21:04:39 -0800

    has updated js code but still won't actually display the result

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,13 @@
-$(document).ready(function(){
-  $("form#leap-year").submit(function(event){
+var leapYear = function(year) {
+  if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+$(document).ready(function() {
+  $("form#leap-year").submit(function(event) {
     event.preventDefault();
     var year = parseInt($("input#year").val());
     var result = leapYear(year);
